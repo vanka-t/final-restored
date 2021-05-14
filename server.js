8fc62588a735101a02c5aa63d72e540c91ad085d
vanka-t <vt2068@nyu.edu>
2021-05-14 08:02:35 -0400

    pasting stuff from pevious final folder

--- server.js
+++ server.js
@@ -5,11 +5,28 @@ var express = require('express');
 
 
 // app = application
-var app = express();
+var app = express(); //creating express app
 
   var server = app.listen(3000);
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000 //linking to local port
 //var server = app.listen(port);
 
 
 app.use(express.static('public'));
+
+var socket = require('socket.io');
+
+var io = socket(server); //variable keeping track of inputs/outputs
+io.sockets.on('connection',newConnection);
+
+function newConnection(socket){ //callback
+    console.log(socket);
+   
+    socket.on('startGame', gameMsg);
+
+    function gameMsg(data){
+        //  console.log(data);
+        socket.broadcast.emit('startGame', data);
+        }
+
+}   
